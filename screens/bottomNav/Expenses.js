c6fdd268fd51b5a92062f867d7d2795bb1e1026e
Younext <youneshaddam9@gmail.com>
2021-01-20 11:54:31 +0100

    'test'

--- screens/bottomNav/Expenses.js
+++ screens/bottomNav/Expenses.js
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 export default function expenses() {
   return (
     <View>
-      <Text>What to do here : calculate expenses</Text>
+      <Text>What to do here : test expenses</Text>
     </View>
   );
 }
