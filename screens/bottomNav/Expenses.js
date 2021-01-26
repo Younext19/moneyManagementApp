56573e229047574887985ea563e4cddc25eafc4e
Younext <youneshaddam9@gmail.com>
2021-01-26 13:23:52 +0100

    small changes

--- screens/bottomNav/Expenses.js
+++ screens/bottomNav/Expenses.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {useSelector} from 'react-redux';
 
 export default function expenses() {
   return (
-    <View>
-      <Text>What to do here : test expenses</Text>
+    <View style={{flex: 1}}>
+      <View style={{height: 60, backgroundColor: 'red'}}>
+        <View
+          style={{
+            height: 40,
+            margin: 15,
+            backgroundColor: 'green',
+            borderRadius: 7,
+          }}></View>
+      </View>
     </View>
   );
 }
